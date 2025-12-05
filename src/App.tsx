990d55fe37523ad338a92062f66faf04d00714d0
Sudhanshu Jha <150656081+Sudhanshu012588@users.noreply.github.com>
2025-12-05 03:41:44 +0000

    asdasd

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <>
-      <a href="http://209.137.198.187:8000/eval">http://209.137.198.187:8000/eval</a>
+      <a href="http://209.137.198.187:8000/eval">Eval path -- http://209.137.198.187:8000/eval</a>
+      <a href="http://209.137.198.187:8000/">VITE_GLOBAL_BASE_URL -- http://209.137.198.187:8000/</a>
     </>
   )
 }
