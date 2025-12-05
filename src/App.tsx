206a27758ae08253efd8c51ed791e8ba7e861421
Sudhanshu Jha <150656081+Sudhanshu012588@users.noreply.github.com>
2025-12-05 03:33:13 +0000

    commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <a href="http://209.137.198.187:8000/eval">http://209.137.198.187:8000/eval</a>
+    </>
+  )
+}
+
+export default App
